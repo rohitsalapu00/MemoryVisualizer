af520180ba2aadb785d73b4f797d4e519c67bb64
Vamsiram07 <vamsiramsalla729@gmail.com>
2025-11-12 13:30:06 +0530

    updated home page and added back to home button

--- gui.py
+++ gui.py
@@ -19,119 +19,183 @@ TEXT_COLOR = "#ffffff"
 style = ttk.Style()
 style.theme_use("clam")
 
-#General style settings
-style.configure(
-    "TLabel",
-    background=CARD_BG,
-    foreground="white",
-    font=("Segoe UI",11)
-)
-style.configure(
-    "TButton",
-    font=("Segoe UI Semibold",11),
-    background=ACCENT_COLOR,
-    foreground="white",
-    padding=10
-)
-style.map("TButton",
-          background=[("active","#6ca0f7")],
-          relief=[("pressed","sunken")])
+style.configure("TLabel",background=CARD_BG, foreground= "white",font=("Segoe UI",11))
+style.configure("TButton", font=("Segoe UI semibold",11), backgound=ACCENT_COLOR,foreground="white",padding=10)
+style.map("TButton", background=[("active","#6ca0f7")], relief=[("pressed", "sunken")])
+# style.map("TButton", background=[("active", "#6ca0f7")], relief=[("pressed", "sunken")])
 
-#Home page
-home_frame = tk.Frame(root,bg=BG_COLOR)
+
+
+#============home page===============
+
+home_frame = tk.Frame(root, bg=BG_COLOR)
 home_frame.pack(fill="both",expand=True)
 
 title_label = tk.Label(
     home_frame,
-    text="Digital Memory Management Visualizer",
+    text="Digital Memory Management visualizer",
     font=("Segoe UI Black",24,"bold"),
     bg=BG_COLOR,
     fg=ACCENT_COLOR
 )
-title_label.pack(pady=80)
+
+title_label.pack(pady=60)
 
 desc_label = tk.Label(
     home_frame,
     text=(
-        "Simulate and visualize how memory pages are replaced "
+        "Simulate and visualize how memory pages are replaced"
         "using FIFO and LRU algorithms.\n\n"
-        "Understand page faults, hits and memory behavior interactively."
+        "understand page faults, hits, and memory behavior interactively."
     ),
-    font=("Segoe UI",13),
+    font=("Segeo UI", 13),
     bg=BG_COLOR,
     fg="#cccccc",
     justify="center"
 )
-desc_label.pack(pady=15)
+
+desc_label.pack(pady=10)
+
+
+
+
+#--Fifo & LRu Explanation-----------
+
+info_frame = tk.Frame(home_frame,bg=CARD_BG, bd=1, relief="solid")
+info_frame.pack(padx=80, pady=30, fill="x")
+
+fifo_label = tk.Label(
+    info_frame,
+    text="Fifo (First In, First Out):",
+    font=("Segoe UI semibold",13),
+    bg=CARD_BG,
+    fg=ACCENT_COLOR,
+    anchor="w"
+)
+
+fifo_label.pack(anchor="w",padx=15, pady=(10,0))
+
+fifo_text = tk.Label(
+    info_frame,
+    text=". The oldest loaded page is replaced first.\n"
+    ". Works like a queue - first page is the first to be removed.\n"
+    ". simple but may cause more page faults (Belady's anomaly).",
+
+    font=("Segoe UI",11),
+    bg=CARD_BG,
+    fg="#cccccc",
+    justify="left",
+    wraplength=700
+)
+
+fifo_text.pack(anchor="w",padx=30,pady=(0,15))
+
+lru_label = tk.Label(
+    info_frame,
+    text = "LRU (Least Recently used):",
+    font=("Segoe UI semibold",13),
+    bg = CARD_BG,
+    fg=ACCENT_COLOR,
+    anchor="w"
+)
+
+lru_label.pack(anchor="w",padx=15, pady=(5,0))
+
+lru_text = tk.Label(
+    info_frame,
+    text=". Replaces the page that has been used for the longest time.\n"
+        ". keeps track of recently accessed pages.\n"
+        ". more efficient than FIFO is most real systems.",
+    font=("Segoe UI",11),
+    bg=CARD_BG,
+    fg="#cccccc",
+    justify="left",
+    wraplength=700
+)
+
+lru_text.pack(anchor="w", padx=30, pady=(0,15))
+
+#start button
 
 start_btn = ttk.Button(
     home_frame,
-    text="Start Simulation",
+    text = "start Simulation",
     command=lambda: show_simulation_page(),
 )
-start_btn.pack(pady=40)
 
-#Simulation page frame
-sim_frame = tk.Frame(root,bg=BG_COLOR)
+start_btn.pack(pady=30)
+
+#--------------------SIMULATION PAGE---------------------------
+
+sim_frame = tk.Frame(root, bg=BG_COLOR)
 
 header = tk.Label(
     sim_frame,
-    text="Simualtion Setup",
-    font=("Segoe UI Semibold", 18, "bold"),
+    text = "Simulation Setup",
+    font=("Segoe UI semibold",18,"bold"),
     bg=BG_COLOR,
     fg=ACCENT_COLOR
 )
-header.pack(padx=30)
 
-#frame for inputs
-input_frame = tk.Frame(sim_frame, bg=BG_COLOR, bd=2, relief="ridge")
-input_frame.pack(pady=20, padx=20, ipadx=20, ipady=20)
+header.pack(padx=30, pady=(20,10))
 
+input_frame = tk.Frame(sim_frame, bg=BG_COLOR, bd = 2,relief="ridge")
+input_frame.pack(pady=20, padx=20, ipadx=20,ipady=20)
 
-#Acess sequence 
-ttk.Label(input_frame, text="Access Sequence(comma separated):").grid(row=0, column=0, sticky="w",padx=10,pady=10)
-access_entry = ttk.Entry(input_frame, width=45)
-access_entry.grid(row=0, column=1,padx=10,pady=10)
+ttk.Label(input_frame, text="Access sequence (comma sepearated):").grid(row=0,column=0, sticky="W",padx=10, pady=10)
+access_entry = ttk.Entry(input_frame,width=45)
+access_entry.grid(row=0, column=1,padx=10, pady=10)
 
-#Number of frames
-ttk.Label(input_frame, text="Number of frames: ").grid(row=1, column=0, sticky="w",padx=10,pady=10)
+ttk.Label(input_frame, text="Number of frames: ").grid(row=1,column=0,sticky="W",padx=10,pady=10)
 frames_entry = ttk.Entry(input_frame, width=20)
-frames_entry.grid(row=1, column=1,padx=10,pady=10)
+frames_entry.grid(row=1,column=1,padx=10,pady=10)
 
-#Replacement algorithm
-ttk.Label(input_frame, text="Algorithm:").grid(row=2, column=0, sticky="w",padx=10, pady=10)
+ttk.Label(input_frame,text="Algorithm:").grid(row=2, column=0,sticky="w",padx=10,pady=10)
 algo_var = tk.StringVar(value="FIFO")
-algo_menu = ttk.OptionMenu(input_frame, algo_var, "FIFO", "FIFO", "LRU")
-algo_menu.grid(row=2, column=1,padx=10, pady=10)
+algo_menu = ttk.OptionMenu(input_frame,algo_var,"FIFO","FIFO","LRU")
+algo_menu.grid(row=2, column=1, padx=10, pady=10)
 
-#Buttons with hover effect
-def on_enter(e):
-    e.widget.config(background="#6ca0f7")
-def on_leave(e):
-    e.widget.config(background=ACCENT_COLOR)
+#hover effects
+
+def on_enter(e): e.widget.config(backgorund="#6ca0f7")
+def on_leave(e): e.widget.config(background=ACCENT_COLOR)
+
+#start simulation button
 
 start_sim_btn = tk.Button(
     input_frame,
-    text="Start Simulation",
+    text="Start simulation",
     bg=ACCENT_COLOR,
     fg="white",
-    font=("Segoe UI Semibold",12),
+    font=("Segoe UI semibold",12),
     width=20,
     relief="flat",
     command=lambda: start_simulation()
 )
 
 start_sim_btn.grid(row=3, column=0, columnspan=2, pady=15)
-start_sim_btn.bind("<Enter>",on_enter)
+start_sim_btn.bind("<Enter>", on_enter)
 start_sim_btn.bind("<Leave>",on_leave)
 
-#frame to display memory
-memory_frame = tk.Frame(sim_frame, bg=BG_COLOR)
+
+#back to home button 
+back_btn = tk.Button(
+    sim_frame,
+    text = "<- Back To Home",
+    bg="#33384d",
+    fg="white",
+    font=("Segoe UI semibold",11),
+    width=15,
+    relief="flat",
+    command=lambda: show_home_page()
+)
+back_btn.pack(pady=10)
+
+#memory visualization area
+memory_frame=tk.Frame(sim_frame,bg=BG_COLOR)
 memory_frame.pack(pady=40)
 
-#funtion to create memory blocks
 memory_labels = []
-
 
 def create_memory_blocks(num_frames):
     #Create empty memory blocks in GUI
@@ -205,7 +269,9 @@ def show_simulation_page():
     home_frame.pack_forget()
     sim_frame.pack(fill="both", expand=True)
 
+def show_home_page():
+    sim_frame.pack_forget()
+    home_frame.pack(fill="both", expand=True)
 
-
-#Run the window
+#Run the window+ 
 root.mainloop()
